58713284e503ebada062400e903dba612de2db4a
SpaGettys <jgettys@gmail.com>
2018-01-31 00:34:00 -0800

    Initial Commit

---
+++ Classes and Objects/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Classes and Objects
+//
+//  Created by SpaGettys on 2018/01/31.
+//  Copyright © 2018 SpaGettys. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
